d8cbdfecf23c21b3a60f87c2efbbd58ef5cb6763
Vadim Shupan <shupan.vadimka@mail.ru>
2024-07-04 04:45:23 +0300

    init: initial commit

---
+++ src/main/java/net/javapractice/springboot/controller/MessageJsonController.java
@@ -0,0 +1,27 @@
+package net.javapractice.springboot.controller;
+
+import net.javapractice.springboot.dto.User;
+import net.javapractice.springboot.publisher.RabbitMQJsonProducer;
+import net.javapractice.springboot.publisher.RabbitMQProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageJsonController {
+
+    private RabbitMQJsonProducer jsonProducer;
+
+    public MessageJsonController(RabbitMQJsonProducer jsonProducer) {
+        this.jsonProducer = jsonProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody User user){
+        jsonProducer.sendJsonMessage(user);
+        return ResponseEntity.ok("Json message sent to rabbitmq ...");
+    }
+}
